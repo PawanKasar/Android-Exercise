5df2ae5141ab53e2de07c4e7485343b1f92daec0
Troublemaker29 <pkasar123@gmail.com>
2016-07-12 14:07:49 +0530

    fragment with button

--- FragmentExercise/app/src/main/java/com/example/pawans/fragmentexercise/FirstFragment.java
+++ FragmentExercise/app/src/main/java/com/example/pawans/fragmentexercise/FirstFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,17 +14,19 @@ import android.view.ViewGroup;
  */
 public class FirstFragment extends Fragment {
 
+    Button btnsecond;
 
     public FirstFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_first, container, false);
+
+        btnsecond = (Button)view.findViewById(R.id.button_2);
 
         return view;
     }
--- FragmentExercise/app/src/main/java/com/example/pawans/fragmentexercise/MainActivity.java
+++ FragmentExercise/app/src/main/java/com/example/pawans/fragmentexercise/MainActivity.java
@@ -4,23 +4,33 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TextView textView;
+    Button btnfirst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //textView = (TextView)findViewById(R.id.textView);
+        btnfirst = (Button)findViewById(R.id.button_1);
 
-        FirstFragment fragment = new FirstFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.fragment_first,fragment);
-        ft.commit();
+        btnfirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirstFragment fragment = new FirstFragment();
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.add(R.id.fragment_first,fragment);
+                ft.commit();
+
+            }
+        });
+
     }
 }
